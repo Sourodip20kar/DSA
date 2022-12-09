db129f66219a5c24541ea343a9e6c4cd2c9d74c6
Sourodip Kar <51.sourodipkar2002n.i.t@gmail.com>
2022-12-09 11:24:45 +0530

    Update linked_list.c

--- linked_list.c
+++ linked_list.c
@@ -83,3 +83,32 @@ current = current->next;
 }
 }
 }
+void printList(struct node* node) {
+while (node != NULL) {
+printf(" %d ", node->data);
+node = node->next;
+}
+}
+
+int main() {
+struct node* head = NULL;
+insertAtEnd(&head, 1);
+insertAtBeginning(&head, 2);
+insertAtBeginning(&head, 3);
+insertAtEnd(&head, 4);
+insertAfter(head->next, 5);
+printf("Linked list: ");
+printList(head);
+printf("\nAfter deleting an element: ");
+deleteNode(&head, 3);
+printList(head);
+int item_to_find = 3;
+if (searchNode(&head, item_to_find)) {
+printf("\n%d is found", item_to_find);
+} else {
+printf("\n%d is not found", item_to_find);
+}
+sortLinkedList(&head);
+printf("\nSorted List: ");
+printList(head);
+}
